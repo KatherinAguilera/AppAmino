012b918e01edbd59522b4a7ecaf6a16c8a493ccf
Katherine Aguilera <katherinam35@gmail.com>
2016-04-25 18:48:38 -0500

    Inicializacion de App Amino con PHP y Laravel

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+class MovieController extends Controller
+{
+        public function __construct(){
+        $this->beforeFilter('@find',['only' => ['edit','update','destroy']]);
+    }
+
+    public function find(Route $route){
+        $this->movie = Movie::find($route->getParameter('movie'));
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $movie = Movie::all();
+            return response()->json($genres);
+        }
+        return view('movie.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('movie.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($request->ajax()){
+            Movie::create($request->all());
+            return response()->json([
+                "mensaje" => "creado"
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return response()->json($this->movie);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->movie->fill($request->all());
+        $this->movie->save();
+        return response()->json(["mensaje" => "listo"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->movie->delete();
+        return response()->json(["mensaje"=>"borrado"]);
+    }
+}
